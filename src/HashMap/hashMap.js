e319b5814e05027aab1ae02528d6cc96b009861d
Ling Q Meng <lingqingmeng@users.noreply.github.com>
2021-01-12 12:29:27 -0800

    Update hash map

--- src/HashMap/hashMap.js
+++ src/HashMap/hashMap.js
@@ -1,9 +1,35 @@
-module.exports = (function (){
-  function HashMap(){
+const Tuple = require('./tuple');
+
+
+class SimpleMap {
+  constructor() {
+    this.map_object = {};
   }
+
+  // key must be type Tuple
+  get(key) {
+    let found = this.map_object[key.getHashCode()]
+    if (typeof found === 'undefined') { return -1 }
+    return found;
+  }
+
+  // key must be string of type x0y0
+  put(key,val) {
+    this.map_object[key.getHashCode()] = val;
+  }
+}
+
+// consider extending SimpleMap with different parameterizations
+function HashMap(){
+}
+
+HashMap.prototype.logTestData = function (){
+  return 'Test Data'
+}
+
+
+module.exports = {
+  SimpleMap, HashMap
+}
   
-  HashMap.prototype.sayHello = function (){
-    return 'My Life for Auir!'
-  }
-  return HashMap
-})()
+  
